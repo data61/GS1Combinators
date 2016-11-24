c59f3623e5930a4ac800e5e79596e0fcabc12dfb
zhu020 <John.Zhu@data61.csiro.au>
2016-11-24 16:15:34 +1100

    improve the implementation of Location - implement GLN (global location number) - implement GLN validation - add GLNException - add .idea in gitignore

---
+++ src/Data/GS1/GS1Exception.hs
@@ -0,0 +1,10 @@
+module Data.GS1.GS1Exception where
+
+import Control.Exception
+import Data.Typeable
+
+-- |Type GLNException
+data GLNException = InvalidGLNLengthException String
+  deriving (Show, Typeable)
+
+instance Exception GLNException
--- test/Tests/Location.hs
+++ test/Tests/Location.hs
@@ -1,10 +1,23 @@
 module Tests.Location where
 
+import Control.Exception (evaluate)
 import Test.Hspec
 import Data.GS1.Location
 
-testPassLocId :: Spec
-testPassLocId = do
+testPassGLN :: Spec
+testPassGLN = do
   describe "Location" $ do
-    it "loc id is passed correctly" $
-      getLocId (ReadPointLocation "934858501.001.xxx") `shouldBe` "934858501.001.xxx"
+    it "GLN is verified correctly" $
+      show (gln "0614141" "18133" "9") `shouldBe` "0614141.18133.9"
+
+    it "GLN is verified correctly" $
+      show (gln "0532132" "14112" "7") `shouldBe` "0532132.14112.7"
+
+    it "InvalidGLNLengthException is caught" $
+      evaluate (gln "0614141" "1813392322222222222" "2") `shouldThrow` anyException
+
+    it "InvalidGLNLengthException is caught" $
+      evaluate (gln "" "" "") `shouldThrow` anyException
+
+    it "Exception caused by invalid character is caught" $
+      evaluate (gln "0614141" "181ab" "9") `shouldThrow` anyException
