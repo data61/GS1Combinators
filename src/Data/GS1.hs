e40c38365a70f79684ae75864f314c0f413c589a
Sara Falamaki <Sara.Falamaki@csiro.au>
2016-12-01 15:52:19 +1100

    Comments describing the fields in each event type

--- src/Data/GS1.hs
+++ src/Data/GS1.hs
@@ -5,17 +5,6 @@ module Data.GS1 where
 import           Data.GS1.Location
 import           GHC.Generics
 
-data Event = Event What When Where --Why
-
--- Verbs
-
-newtype What = What [URI] deriving (Show,Eq,Generic)
-type    When = EPCISTime
-data    Where = Where ReadPointLocation BusinessLocation deriving (Show,Eq,Generic)
--- newtype ReadPointLocation = RP Location deriving (Show,Eq,Generic)
--- newtype BuisinessLocation = Biz Location deriving (Show,Eq,Generic)
-data Action = Add | Observe | Delete
-
 -- TODO implement these
 data URI = URI deriving (Show,Eq,Generic)-- URN Namespace Payload |EPC |URI Namespace Payload deriving (Show,Eq,Generic)
 type Payload = String
@@ -50,52 +39,6 @@ data BusinessTransactionType = Bol | Desadv | Inv | Pedigree | Po | Poc
     deriving (Show,Eq,Generic)
 
 
--- data What = ObjectEvent [EPCObject] | AggregationEvent (Maybe Parent) [Child] | TransactionEvent ??
---   | TransformationEvent Input Output TransformationID
-
--- newtype Input = [EPCObject]
--- newtype Output = [EPCObject]
-
-
--- Valid Dispositions, defined in section CBV 7.2
-dispositionValidList :: Disposition -> [BusinessStep]
-dispositionValidList Active           =  [Commissioning]
-dispositionValidList Container_Closed =  [Staging_Outbound]
-dispositionValidList Damaged          =  [Accepting, Inspecting, Receiving, Removing, Repairing, Replacing]
-dispositionValidList Destroyed  =  [Destroying]
-dispositionValidList Dispensed  =  [] -- nothing defined - page 25 of spec
-dispositionValidList Encoded    =  [Encoding]
-dispositionValidList Expired    =  [Holding, Staging_Outbound, Storing]
-dispositionValidList In_Progress=  [Receiving, Picking, Loading, Accepting, Staging_Outbound, Arriving, Void_Shipping]
-dispositionValidList In_Transit =  [Shipping, Departing]
-dispositionValidList Inactive   =  [Decommissioning]
-dispositionValidList No_Pedgree_Match   =  [Holding, Staging_Outbound, Storing]
-dispositionValidList Non_Sellable_Other =  [Holding, Inspecting, Staging_Outbound, Storing]
-dispositionValidList Partially_Dispensed=  []  -- nothing defined - page 25 of spec
-dispositionValidList Recalled   =  [Holding, Staging_Outbound, Storing]
-dispositionValidList Reserved   =  [Reserving]
-dispositionValidList Retail_Sold=  [Retail_Selling]
-dispositionValidList Returned   =  [Receiving, Holding, Shipping]
-dispositionValidList Sellable_Accessible     =  [Stocking, Receiving]
-dispositionValidList Sellable_Not_Accessible =  [Receiving, Storing, Loading, Holding, Inspecting]
-dispositionValidList Stolen =  [] -- nothing defined - page 25 of spec
-dispositionValidList Unknown =  [] -- nothing defined - page 25 of spec
-
-dispositionValidFor :: BusinessStep -> Disposition -> Bool
-dispositionValidFor bs disp = bs `elem` dispositionValidList disp
-
-{-
--- The why smart constructor
--- Have to make sure the disposition is valid for that particular business
--- step.
-why :: BusinessStep -> Disposition -> [BusinessTransactionReference] -> [SrcDestReference] -> Why
-why step disp trans srcdsts =
-    if dispositionValidFor step disp
-    then Why step disp trans srcdsts
-    else error $ "Disposition not valid for business step. " ++
-                  " Valid BusinessSteps for " ++ show disp ++ "include: " ++
-                  show (dispositionValidList disp)
--}
 
 
 -- URIs!
@@ -116,9 +59,9 @@ why step disp trans srcdsts =
 
 
 
-data SrcDestReference = SDR SrcDestType Where deriving (Show,Eq,Generic)
+--data SrcDestReference = SDR SrcDestType Where deriving (Show,Eq,Generic)
 
-data SrcDestTypeURI = SDTU Payload deriving (Show,Eq,Generic)
+--data SrcDestTypeURI = SDTU Payload deriving (Show,Eq,Generic)
 
 -- These are the Source/Dest Type URI payloads
 -- urn:epcglobal:cbv:sdt:payload
