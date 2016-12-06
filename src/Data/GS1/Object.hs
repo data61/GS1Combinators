43b85cc5a54713bbb6161858eb03a68a824bd8a8
zhu020 <John.Zhu@data61.csiro.au>
2016-12-06 18:29:26 +1100

    use only simple representation of the quantity the implementation will be expanded if there are more operations on it

--- src/Data/GS1/Object.hs
+++ src/Data/GS1/Object.hs
@@ -4,6 +4,14 @@ module Data.GS1.Object where
 import Data.GS1.EPC
 import Data.GS1.URI
 
+-- |TODO expand it to the proper implementation when necessary
+type Amount = Double
+
+type Unit = String
+
+-- |Simple quantity representation
+data Quantity = Quantity Unit Amount
+
 -- |EPCIS 1.0
 data Object = PhysicalObject { _id :: ObjectID } | DigitalObject { _id :: ObjectID }
 
@@ -11,13 +19,14 @@ data Object = PhysicalObject { _id :: ObjectID } | DigitalObject { _id :: Object
 -- |Ref: CBV 8.2 & 8.3, EPCIS 1.0
 data ObjectID = InstanceLevelID {
                  _epcList    :: [EPC]
-               , _parentID   :: ObjectID
+               , _parentID   :: Maybe[ObjectID]
                , _childEPCs  :: [EPC]
                , _inputEPCS  :: [EPC]
                , _outputEPCS :: [EPC]
                }
                | ClassLevelID {
                  _epcClass :: EpcClass
+               , _quantity :: Quantity
                }
 
 -- |Any identifiable object will be identified by ObjectID
