39ee92118996cabcc1fc89f0bda73864d642aaa7
Sara Falamaki <Sara.Falamaki@csiro.au>
2017-01-11 14:25:46 +1100

    added stub for parser, and put xml-conduit in the cabal file

---
+++ src/Data/GS1/Parser/Parser.hs
@@ -0,0 +1,7 @@
+module Data.GS1.Parser.Parser where
+
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+--import qualified Data.Map        as M
+import           Prelude         hiding (readFile, writeFile)
