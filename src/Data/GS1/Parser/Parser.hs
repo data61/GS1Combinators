9f81dfcac3ab8b78b41009c69826c16be3f90e78
Sara Falamaki <Sara.Falamaki@csiro.au>
2017-01-17 16:37:44 +1100

    Not much here yet, just a starting point
    Former-commit-id: b303569a09b609309e4c71983c572d53f117c2bf

--- src/Data/GS1/Parser/Parser.hs
+++ src/Data/GS1/Parser/Parser.hs
@@ -7,3 +7,23 @@ import           Prelude         hiding (readFile, writeFile)
 import qualified Data.Map        as M
 --import           Text.Hamlet.XML
 import           Text.XML
+import Data.GS1.Event
+
+--parse IO :: ()
+--"/Users/fal05c/work/gs1combinators/src/Data/GS1/Parser/ObjectEvent.xml"
+parse :: [Event] -> FilePath -> IO ()
+parse eventList filename = do
+    -- readFile will throw any parse errors as runtime exceptions
+    -- def uses the default settings
+    Document prologue root epilogue <- readFile def filename
+    -- root is the root element of the document
+    --
+    print prologue
+    print "-----------"
+    print root
+    print "-----------"
+    print epilogue
+    print "-----------"
+
+
+
