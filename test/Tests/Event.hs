323cae9f2c25962d04e56a4955681542ba5cfde5
zzhu <john.zhu@data61.csiro.au>
2017-01-24 13:38:33 +1100

    implement mkDWhen
    valid EventTime will create a DWhen, the function asssumes that if parseStr2Time works
    on a certain string, parseStr2TimeZone must also work.
    
    Invalid RecordTime will be ignored. Valid RecordTime should always be greater than EventTime
    
    
    Former-commit-id: f4748f28c485828fb23c327c102bdc435fd89ec7

--- test/Tests/Event.hs
+++ test/Tests/Event.hs
@@ -1,11 +1,16 @@
 module Tests.Event where
 
+import           Data.Either.Combinators
 import           Data.GS1.BizStep
 import           Data.GS1.BizTransaction
 import           Data.GS1.Disposition
 import           Data.GS1.DWhat
 import           Data.GS1.EPC
+import           Data.GS1.EPCISTime
+import           Data.GS1.Event
 import           Data.GS1.URI
+import           Data.GS1.Utils
+import           Data.Time.LocalTime
 import           Test.Hspec
 
 testBizStep :: Spec
@@ -53,8 +58,8 @@ testBizTransaction =
     it "parse the empty uri to Nothing" $
       parseBizTransactionType "" `shouldBe` Nothing
 
-testCreateDWhat :: Spec
-testCreateDWhat = do
+testMkDWhat :: Spec
+testMkDWhat = do
   describe "create valid ObjectDWhat" $
     it "creates DWhat from valid input" $
       ppDWhat (ObjectDWhat Observe [GLN "urn:epc:id:sgtin:0614141" "107346" "2017", GLN "urn:epc:id:sgtin:0614141" "107346" "2018"] []) `shouldBe` "OBJECT WHAT\nObserve\n[urn:epc:id:sgtin:0614141.107346.2017,urn:epc:id:sgtin:0614141.107346.2018]\n[]"
@@ -64,3 +69,52 @@ testCreateDWhat = do
 
   -- TODO test create other DWhat too
 
+testMkDWhen :: Spec
+testMkDWhen = do
+  describe "create valid DWhen" $ do
+    it "creates DWhen from two valid time Strngs" $ do
+      let et = "2005-04-03T20:33:31.116-06:00"
+      let rt = "2005-04-03T20:39:47.16-06:00"
+
+      let pet = fromRight' (parseStr2Time et :: Either EPCISTimeError EPCISTime)
+      let prt = fromRight' (parseStr2Time rt :: Either EPCISTimeError EPCISTime)
+      let tz = fromRight' (parseStr2TimeZone et :: Either EPCISTimeError TimeZone)
+      let dwhen = DWhen pet (Just prt) tz
+
+      mkDWhen et rt `shouldBe` Just dwhen
+    
+    it "event time should be smaller than record time" $ do
+      let et = "2005-04-03T20:39:47.16-06:00"
+      let rt = "2005-04-03T20:33:31.116-06:00"
+
+      mkDWhen et rt `shouldBe` Nothing
+
+    it "ignores the record time" $ do
+      let et = "2005-04-03T20:33:31.116-06:00"
+      let rt = "2005-04-03T20:39:47.16-06:00-invalid-format"
+
+      let pet = fromRight' (parseStr2Time et :: Either EPCISTimeError EPCISTime)
+      let tz = fromRight' (parseStr2TimeZone et :: Either EPCISTimeError TimeZone)
+      let dwhen = DWhen pet Nothing tz
+
+      mkDWhen et rt `shouldBe` Just dwhen
+
+    it "does not ignore invalid event time" $ do
+      let et = "2005-04-3T20:39:47.16-06:00-invalid-format"
+      let rt = "2005-04-3T20:33:31.116-06:00"
+
+      mkDWhen et rt `shouldBe` Nothing
+  
+  describe "create valid DWhen by mkDWhen'" $ do
+    it "creates valid DWhen from one time string" $ do
+      let t = "2017-01-24T13:08:24.11+10:00"
+      let pt = fromRight' (parseStr2Time t :: Either EPCISTimeError EPCISTime)
+      let tz = fromRight' (parseStr2TimeZone t :: Either EPCISTimeError TimeZone)
+
+      let dwhen = DWhen pt (Just pt) tz
+
+      mkDWhen' t `shouldBe` Just dwhen
+    
+    it "fails on invalid string" $ do
+      let t = "invalid input"
+      mkDWhen' t `shouldBe` Nothing
