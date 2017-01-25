f62f0a964e5edc970a847bab8926c87bbbeb7bc5
zhu020 <John.Zhu@data61.csiro.au>
2017-01-25 12:37:07 +1100

    tweak functions

--- src/XML/Parser.hs
+++ src/XML/Parser.hs
@@ -5,12 +5,15 @@ module XML.Parser where
 import           Data.GS1.EPCISTime
 import           Data.GS1.Event
 import           Data.GS1.Utils
+import           Data.List.Split
 import           Data.Maybe
 import qualified Data.Text           as T
 import           Data.Time.LocalTime
+import           Data.XML.Types
+import           Text.Read
 import           Text.XML.Cursor
 
--- Only the first occurance of EventTime for each Event will be recognised
+-- |Only the first occurance of EventTime for each Event will be recognised
 parseTimeXML :: [T.Text] -> Maybe EPCISTime
 parseTimeXML t = case t of
                    (x:_) -> let pt = parseStr2Time (T.unpack x) :: Either EPCISTimeError EPCISTime in
@@ -19,6 +22,7 @@ parseTimeXML t = case t of
                                    Right a -> Just a
                    _      -> Nothing
 
+-- |Only the first occurrance of EventTime for each Event will be recognised
 parseTimeZoneXML :: [T.Text] -> Maybe TimeZone
 parseTimeZoneXML t = case t of
                        (x:_) -> let ptz = parseStr2TimeZone (T.unpack x) :: Either EPCISTimeError TimeZone in
@@ -27,17 +31,31 @@ parseTimeZoneXML t = case t of
                                        Right a -> Just a
                        _      -> Nothing
 
--- | the name of the current cursor stays at ObjectEvent
+-- |Parse TimeZone from eventTimeZoneOffset
+-- Only the first occured TimeZone will be considered
+parseTimeZoneXML' :: [T.Text] -> Maybe TimeZone
+parseTimeZoneXML' [] = Nothing
+parseTimeZoneXML' (t:_) = let l = splitOn ":" (T.unpack t) in
+                              case l of
+                                (x:_) -> let rx = readMaybe x :: Maybe Int in
+                                             case rx of
+                                               Just t'  -> Just $ hoursToTimeZone t'
+                                               Nothing  -> Nothing
+                                _      -> Nothing
+
+-- |The name of the current cursor stays at ObjectEvent
 parseDWhen :: Cursor -> Maybe DWhen
 parseDWhen c = do
   let etn = c $/ element "eventTime" &/ content
   let rtn = c $/ element "recordTime" &/ content
+  let tzn = c $/ element "eventTimeZoneOffset" &/ content
   let et = parseTimeXML etn
   let rt = parseTimeXML rtn
-  let tz = parseTimeZoneXML etn
+  let tz = if isNothing $ parseTimeZoneXML' tzn then parseTimeZoneXML' tzn else parseTimeZoneXML etn
   case et of
     Just et' -> Just (DWhen et' rt (fromJust tz))
     _        -> Nothing
 
-objectEventCursors :: Cursor -> [Cursor]
-objectEventCursors c = c $// element "ObjectEvent"
+-- |Get all the cursors with the given name below the current cursor
+getCursorsByName :: Name -> Cursor -> [Cursor]
+getCursorsByName n c = c $// element n
--- test/Tests/Parser.hs
+++ test/Tests/Parser.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Tests.Parser where
 
 import           Data.GS1.Event
@@ -8,13 +10,20 @@ import           XML.Parser
 import           Test.Hspec
 
 testParseDWhen :: Spec
-testParseDWhen = do
-  describe "parse XML to obtain DWhen" $
+testParseDWhen =
+  describe "parse XML to obtain DWhen" $ do
     it "creates DWhen from valid XML" $ do
       doc <- Text.XML.readFile def "test/test-xml/ObjectEvent.xml"
       let cursor = fromDocument doc
-      let oeCursors = objectEventCursors cursor
+      let oeCursors = getCursorsByName "ObjectEvent" cursor
       --mapM_ print $ parseDWhen <$> oeCursors
       let et = "2005-04-03T20:33:31.116-06:00"
       let et1 = "2005-04-04T20:33:31.116-06:00"
       parseDWhen <$> oeCursors `shouldBe` [mkDWhen' et, mkDWhen et1 ""]
+
+    it "creates Nothing from invalid XML when there is no Event Time" $ do
+      doc <- Text.XML.readFile def "test/test-xml/ObjectEventNoEventTime.xml"
+      let cursor = fromDocument doc
+      let oeCursors = getCursorsByName "ObjectEvent" cursor
+      parseDWhen <$> oeCursors `shouldBe` [Nothing]
+
