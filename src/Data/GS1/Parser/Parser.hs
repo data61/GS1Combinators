d290042eb49230f4d8634ae90a6de5dac7d152ed
zhu020 <John.Zhu@data61.csiro.au>
2017-02-06 14:10:19 +1100

    relocate the Parser and place the example xml elsewhere
    Former-commit-id: 667bd769ee99dc3c92eeb3909f37222625152a0f

--- src/Data/GS1/Parser/Parser.hs
+++ src/Data/GS1/Parser/Parser.hs
@@ -1,194 +1,206 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
 
 module Data.GS1.Parser.Parser where
-import           Prelude         hiding (readFile, writeFile)
-import Data.Time
-import Data.Time.Format
 
-import qualified Data.Map        as M
-import           Text.XML
-import Data.GS1.Event
-import Data.GS1.Location
-import Data.GS1.SourceDest
-import Data.Maybe
+import           Control.Lens         hiding (element)
+import           Data.GS1.BizStep
+import           Data.GS1.Disposition
+import           Data.GS1.DWhat
+import           Data.GS1.DWhy
+import           Data.GS1.EPC
+import           Data.GS1.EPCISTime
+import           Data.GS1.Event
+import           Data.GS1.EventID
+import           Data.GS1.Location
+import           Data.GS1.Object
+import           Data.GS1.Utils
+import           Data.List
+import           Data.List.Split
+import           Data.Maybe
+import qualified Data.Text            as T
+import           Data.Time.LocalTime
+import           Data.UUID
+import           Data.UUID.V1
+import           Data.XML.Types       hiding (Event)
+import           Text.Read
+import           Text.XML.Cursor
 
---parse IO :: ()
---"/Users/fal05c/work/gs1combinators/src/Data/GS1/Parser/ObjectEvent.xml"
-parse :: [Event] -> FilePath -> IO ()
-parse eventList filename = do
-    -- readFile will throw any parse errors as runtime exceptions
-    -- def uses the default settings
-    Document prologue root epilogue <- readFile def filename
-    -- root is the root element of the document
-    --
-    --let eventList = getEvents root
-    --print eventList
-    let eventList = getEventList root
-    print "eventList: --------------------"
-    print eventList
-    print "endEventList: ------------"
-    let timeZoneOffset = fromJust $ searchElement "disposition" (head eventList)
-    print "timezoneoffset: -------------"
-    print timeZoneOffset
-    print "------------------"
-    let when = makeWhen (head eventList)
-    print when
-    print "getBizStep"
-    let readPoint = getDisposition (head eventList)
-    print readPoint
-    --let names = getNames (head eventList)
-    --print names
+-- |Get all the cursors with the given name below the current cursor
+getCursorsByName :: Name -> Cursor -> [Cursor]
+getCursorsByName n c = c $// element n
 
+-- |Only the first occurance of EventTime for each Event will be recognised
+parseTimeXML :: [T.Text] -> Maybe EPCISTime
+parseTimeXML t = case t of
+                   (x:_) -> let pt = parseStr2Time (T.unpack x) :: Either EPCISTimeError EPCISTime in
+                                 case pt of
+                                   Left _  -> Nothing
+                                   Right a -> Just a
+                   _      -> Nothing
 
+-- |Only the first occurrance of EventTime for each Event will be recognised
+parseTimeZoneXML :: [T.Text] -> Maybe TimeZone
+parseTimeZoneXML t = case t of
+                       (x:_) -> let ptz = parseStr2TimeZone (T.unpack x) :: Either EPCISTimeError TimeZone in
+                                     case ptz of
+                                       Left _  -> Nothing
+                                       Right a -> Just a
+                       _      -> Nothing
 
-getEventList :: Element -> [Node]
-getEventList (Element _ _ nodes) = filter isElement events
-  where
-    (NodeElement (Element _ _ bodyNodes)) = head(filter isElement nodes)
-    (NodeElement (Element _ _ events))= head(filter isElement bodyNodes)
+-- |Parse TimeZone from eventTimeZoneOffset
+-- Only the first occured TimeZone will be considered
+parseTimeZoneXML' :: [T.Text] -> Maybe TimeZone
+parseTimeZoneXML' [] = Nothing
+parseTimeZoneXML' (t:_) = let l = splitOn ":" (T.unpack t) in
+                              case l of
+                                (x:_) -> let rx = readMaybe x :: Maybe Int in
+                                             case rx of
+                                               Just t'  -> Just $ hoursToTimeZone t'
+                                               Nothing  -> Nothing
+                                _      -> Nothing
 
+-- |The name of the current cursor stays at ObjectEvent
+parseDWhen :: Cursor -> Maybe DWhen
+parseDWhen c = do
+  let etn = c $/ element "eventTime" &/ content
+  let rtn = c $/ element "recordTime" &/ content
+  let tzn = c $/ element "eventTimeZoneOffset" &/ content
+  let et = parseTimeXML etn
+  let rt = parseTimeXML rtn
+  let tz = if isNothing $ parseTimeZoneXML' tzn then parseTimeZoneXML' tzn else parseTimeZoneXML etn
+  case et of
+    Just et' -> Just (DWhen et' rt (fromJust tz))
+    _        -> Nothing
 
+parseAction :: [T.Text] -> Maybe Action
+parseAction t = case t of
+                  (x:_) -> mkAction . T.unpack $ x
+                  _     -> Nothing
 
-isElement :: Node -> Bool
-isElement (NodeElement e) = True
-isElement _ = False
+parseEPCList :: [T.Text] -> [EPC]
+parseEPCList ts = fromJust <$> (mkEPC "EPC" . T.unpack <$> ts)
 
+parseBizStep :: [T.Text] -> Maybe BizStep
+parseBizStep ts = case ts of
+                    (x:_) -> mkBizStep . T.unpack $ x
+                    _     -> Nothing
 
+parseDisposition :: [T.Text] -> Maybe Disposition
+parseDisposition ts = case ts of
+                        (x:_) -> mkDisposition . T.unpack $ x
+                        _     -> Nothing
 
-----------------------------
--- Why -------------------
--- -------------------------
+parseDWhy :: Cursor -> Maybe DWhy
+parseDWhy c = do
+  let biz = c $/ element "bizStep" &/ content
+  let disp = c $/ element "disposition" &/ content
+  let pbiz = parseBizStep biz
+  let pdisp = parseDisposition disp
+  mkDWhy pbiz pdisp
 
---getBizStep :: Node -> Maybe BizStep
---returns Just "urn:epcglobal:cbv:bizstep:receiving"
-getBizStep node = if (isNothing bizStepNodes || bizNode == [] )
-                     then Nothing else Just bizStepStr
-  where
-    bizStepNodes = searchElement "bizStep" node
-    (Element _ _ bizNode) = head $ fromJust $ bizStepNodes
-    (NodeContent bizStepStr) = head bizNode
-    -- TODO: parse bizStepStr, and return that instead.
+parseQuantity :: Cursor -> Maybe QuantityElement
+parseQuantity c = do
+  let ec = c $/ element "epcClass" &/ content
+  let qt = c $/ element "quantity" &/ content
+  let uom = c $/ element "uom" &/ content
+  case ec of
+    []    -> Nothing
+    (e:_) -> case qt of
+               []    -> Nothing
+               (q:_) -> case uom of
+                          []    -> Nothing
+                          (u:_) -> let [e', q', u'] = T.unpack <$> [e, q, u] in
+                                   Just $ QuantityElement e' (read q' :: Integer) u'
 
+-- | Recusrively construct ObjectDWhat dimension
+parseObjectDWhat :: Cursor -> Maybe DWhat
+parseObjectDWhat c = do
+  -- find action right below ObjectEvent tag
+  let act = c $/ element "action" &/ content
+  -- find all epcs below epcList tag
+  let epc = c $/ element "epcList" &/ element "epc" &/ content
+  -- find all quantityElement, regardless parent tag
+  let qt  = getCursorsByName "quantityElement" c
 
---getDisposition :: Node -> Maybe Disposition
---returns Just "urn:epcglobal:cbv:disp:in_progress"
-getDisposition node = if (isNothing dispNodes || dispNode == [] )
-                     then Nothing else Just dispStr
-  where
-    dispNodes = searchElement "disposition" node
-    (Element _ _ dispNode) = head $ fromJust $ dispNodes
-    (NodeContent dispStr) = head dispNode
-    -- TODO: parse disposition, and return that instead.
+  -- parsed action
+  let pact = parseAction act
+  -- parsed epc
+  let pepc = parseEPCList epc
+  -- parsed quantity
+  let pq = fromJust <$> filter isJust (parseQuantity <$> qt)
 
-
-----------------------------
--- When -------------------
--- -------------------------
-
-
-
---makeWhen :: Node -> (Text, Text)
---FIXME: the parsing doesn't work, and the compiler says it's
---deprecated. Work out what to do! :)
-makeWhen node  = (timeString, tzString)
-  where
-    timeElements = fromJust $ searchElement "eventTime" node
-    timeZoneElements = fromJust $ searchElement "eventTimeZoneOffset" node
-    (Element _ _ tzNode) = head timeZoneElements
-    (NodeContent tzString) = head tzNode --eg: "+02:00"
-    (Element _ _ timeNode) = head timeElements
-    (NodeContent timeString) = head timeNode --eg: "2013-06-08T14:58:56.591Z"
-    time = Data.Time.parseTime defaultTimeLocale
-      "%Y-%m-%dT%H:%M:%S %z" ((take 19 (show timeString))++" "++
-        (show tzString)) :: Maybe UTCTime
-
-----------------------------
--- Where -------------------
--- -------------------------
-
-{-
-makeWhere node = DWhere readPoint bizLocation srcTypes destTypes
-  where
-    readPoint = getReadPoint node
-    bizLocation = getBizLocation node
-    srcDestType = getSrcDestTypes node
--}
-
---returns (Just "urn:epc:id:sgln:0614141.00777.0")
-getReadPoint node = if (isNothing readPointNode) || (isNothing ids)
-                       then Nothing else Just id
-  where
-    readPointNode = searchElement "readPoint" node
-    (Element _ _ rpNode) = head $ fromJust $ readPointNode
-    ids = searchElements "id" rpNode
-    (Element _ _ idNodes) = head $ fromJust $ ids
-    (NodeContent id) = head idNodes
-
---getBizLocation :: Node -> Maybe BizLocation
---returns (Just "urn:epc:id:sgln:0614141.00888.0")
-getBizLocation node = if isNothing $ bizLocationNode
-                         then Nothing else Just id
-  where
-    bizLocationNode = searchElement "bizLocation" node
-    (Element _ _ bizNode) = head $ fromJust $ bizLocationNode
-    ids = searchElements "id" bizNode
-    (Element _ _ idNodes) = head $ fromJust $ ids
-    (NodeContent id) = head idNodes
+  case pact of
+    Nothing -> Nothing
+    Just p  -> Just $ ObjectDWhat p pepc pq
 
 
-getSrcTypes :: Node -> Maybe [SourceDestType]
-getSrcTypes = error "Implement me"
+-- |TODO: due to lack of data, source destination type might not be implemented for now
+-- there could be multiple readpoints and bizlocations
+-- and there could be no srcDest Type involved
+-- the sgln could be irregular
+-- |TODO: there must be some more modification on it
+parseDWhere :: Cursor -> Maybe DWhere
+parseDWhere c = do
+  let rp = c $/ element "readPoint"   &/ element "id" &/ content
+  let bl = c $/ element "bizLocation" &/ element "id" &/ content
+  let rps = (mkLocation . T.unpack) <$> rp
+  let bls = (mkLocation . T.unpack) <$> bl
+  Just $ DWhere rps bls [] []
 
-getDestTypes :: Node -> Maybe [SourceDestType]
-getDestTypes = error "Implement me"
+parseEventList' :: EventType -> [(Maybe EventID, Maybe DWhat, Maybe DWhen, Maybe DWhy, Maybe DWhere)] -> [Maybe Event]
+parseEventList' et l = case l of
+                         []     -> []
+                         (x:xs) -> let i  = x^._1
+                                       w1 = x^._2
+                                       w2 = x^._3
+                                       w3 = x^._4
+                                       w4 = x^._5 in
+                                       if isNothing i  ||
+                                          isNothing w1 ||
+                                          isNothing w2 ||
+                                          isNothing w3 ||
+                                          isNothing w4 then
+                                          Nothing : parseEventList' et xs      else
+                                          mkEvent et (fromJust i) (fromJust w1) (fromJust w2) (fromJust w3) (fromJust w4) : parseEventList' et xs
 
+parseEventID :: Cursor -> Maybe EventID
+parseEventID c = do
+  let eid = c $/ element "eventID" &/ content
+  case eid of
+    []    -> Nothing
+    (x:_) -> let uuid = fromText x in
+                 case uuid of
+                   Nothing -> Nothing
+                   Just u  -> Just $ EventID u
 
-----------------------------
--- Utilities ---------------
-----------------------------
+-- | Find all object events
+parseObjectEvent :: Cursor -> [Maybe Event]
+parseObjectEvent c = do
+  let oeCursors = c $// element "ObjectEvent"
+  let eventID = parseEventID <$> oeCursors
+  let dwhat = parseObjectDWhat <$> oeCursors
+  let dwhen = parseDWhen <$> oeCursors
+  let dwhy = parseDWhy <$> oeCursors
+  let dwhere = parseDWhere <$> oeCursors
+  let zipd = zip5 eventID dwhat dwhen dwhy dwhere
+  --fromJust <$> filter isJust (parseEventList' ObjectEventT zipd)
+  parseEventList' ObjectEventT zipd
 
-
--- search the node's children for a node with a particular Name
--- searchElement :: Name -> Node -> [Element]
-searchElement term node = if result==[] then Nothing else Just result
-  where
-    result = searchElement' term node
-
-searchElement' term (NodeElement e) =
-  if name==term then e:rest else rest
-  where
-    (Element name _ nodes) = e
-    rest = searchElements' term nodes
-
-searchElement' _ (NodeContent t) = []
-searchElement' _ (NodeComment _) = []
-searchElement' _ (NodeInstruction _) = []
-
-searchElements term nodes = if result==[] then Nothing else Just result
-  where
-    result = searchElements' term nodes
-
-searchElements' term nodes = (concat $ map (searchElement' term) nodes)
-
-
-getNames :: Node -> [Name]
-getNames (NodeElement (Element name _ nodes)) = name:(concat $ map getNames nodes)
-getNames (NodeContent t) = []
-getNames (NodeComment _) = []
-getNames (NodeInstruction _) = []
-
-
-{-
-createAggregationEvent :: Node -> Event
-createAggregationEvent NodeElement e =
-  newEvent eventID AggregationEventT what when why whre
-    where
-      when = getWhen e
-
-getWhen :: Node -> DWhen
-getWhen NodeElement e =
-  searchElement "eventTime"
--}
-
-
+parseEventByType :: Cursor -> EventType -> [Maybe Event]
+parseEventByType c et = do
+  let tagS = case et of
+               ObjectEventT         -> "ObjectEvent"
+               AggregationEventT    -> "AggregationEvent"
+               QuantityEventT       -> "QuantityEvent"
+               TransactionEventT    -> "TransactionEvent"
+               TransformationEventT -> "TransformationEvent"
+  let eCursors = c $// element tagS
+  -- TODO
+  let eventID = parseEventID <$> eCursors
+  let dwhat = case et of
+                ObjectEventT        -> parseObjectDWhat <$> eCursors
+  let dwhen = parseDWhen <$> eCursors
+  let dwhy = parseDWhy <$> eCursors
+  let dwhere = parseDWhere <$> eCursors
+  let zipd = zip5 eventID dwhat dwhen dwhy dwhere
+  parseEventList' et zipd
