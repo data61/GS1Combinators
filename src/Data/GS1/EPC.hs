fc648999e8ab3d02e5805d0768cd8f422665bd9d
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2017-11-24 12:22:16 +1100

    Eta reduce
    Former-commit-id: d147ba402c054a59a5cb7e1dd97f5e70480d6350

--- src/Data/GS1/EPC.hs
+++ src/Data/GS1/EPC.hs
@@ -563,7 +563,7 @@ instance ToField TimeZone where
 -- copied from
 -- https://hackage.haskell.org/package/swagger2-2.1.3/docs/src/Data.Swagger.Internal.Schema.html#line-477
 named :: T.Text -> Schema -> NamedSchema
-named name schema = NamedSchema (Just name) schema
+named name = NamedSchema (Just name)
 
 timeSchema :: T.Text -> Schema
 timeSchema fmt = mempty
