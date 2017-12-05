8373cb9a00d08e7abc72fb283105526880b17e5c
Matthew Perry <Matt.Perry@data61.csiro.au>
2017-12-05 16:57:44 +1100

    add to testing
    Former-commit-id: 067822c22e29a4aaab128b395098fa8c74bdc884

--- src/testParser.hs
+++ src/testParser.hs
@@ -7,6 +7,8 @@ import qualified Data.Text as T
 import           Text.XML
 import           Text.XML.Cursor
 
+import Data.GS1.EPC
+
 main :: IO()
 main = do
   doc <- Text.XML.readFile def "../test/test-xml/ObjectEvent.xml"
@@ -22,4 +24,18 @@ main = do
   print $ fromJust . parseDWhere <$> oeCursors
   print $ fromJust . parseObjectDWhat <$> oeCursors
   print $ fromJust . parseAggregationDWhat <$> oeCursors
-  print $ fromJust . parseTransactionDWhat <$> oeCursors
+
+  print $ blahfoo <$> oeCursors -- this shows that parseBizTransaction is bugged - mkBizTransactionType might not work with fromJust
+  -- print $ fromJust . parseTransactionDWhat <$> oeCursors
+
+  ------------------------------------------------------
+blahfoo c = do
+  let texts = c $// element "bizTransaction" &/ content
+  let attrs = foldMap id (c $// element "bizTransaction" &| attribute "type")
+  let z = zip attrs texts
+  parseBizTransactionHelp <$> z
+    where
+      parseBizTransactionHelp (a, b) =
+        mkBizTransactionType (T.unpack . T.strip $ b)
+--        Just $ BizTransaction (T.unpack . T.strip $ a) $
+--          fromJust $ mkBizTransactionType (T.unpack . T.strip $ b)
