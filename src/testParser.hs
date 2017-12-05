f9fa359929880f42670562df826f80fad876f36c
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2017-12-05 15:33:21 +1100

    Add minimal example of parser
    Former-commit-id: 5b3d6835fe130c99d0a39edd08b97f280127a469

---
+++ src/testParser.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Aeson.Encode.Pretty
+import           Data.GS1.Parser.Parser
+import           Data.Maybe
+import qualified Data.Text              as T
+import           Text.XML
+import           Text.XML.Cursor
+
+main :: IO()
+-- main = error "not implemented yet"
+main = do
+  doc <- Text.XML.readFile def "../test/test-xml/ObjectEvent.xml"
+  let cursor = fromDocument doc
+  let oeCursors = getCursorsByName "ObjectEvent" cursor
+  let et = "2005-04-03T20:33:31.116-06:00"
+  let et1 = "2005-04-04T20:33:31.116-06:00"
+  print $ head $ fromJust . parseDWhen <$> oeCursors
+  -- print $ length oeCursors
