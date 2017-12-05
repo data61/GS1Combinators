93e1d89551dda7c4b11210da4585967947db9a01
Matthew Perry <Matt.Perry@data61.csiro.au>
2017-12-05 16:42:34 +1100

    add some manual tests

--- src/testParser.hs
+++ src/testParser.hs
@@ -15,5 +15,12 @@ main = do
   let oeCursors = getCursorsByName "ObjectEvent" cursor
   let et = "2005-04-03T20:33:31.116-06:00"
   let et1 = "2005-04-04T20:33:31.116-06:00"
-  print $ head $ fromJust . parseDWhen <$> oeCursors
+  -- print $ head $ fromJust . parseDWhen <$> oeCursors
   -- print $ length oeCursors
+
+  print $ fromJust . parseDWhen <$> oeCursors
+  print $ fromJust . parseDWhy <$> oeCursors
+  print $ fromJust . parseDWhere <$> oeCursors
+  print $ fromJust . parseObjectDWhat <$> oeCursors
+  print $ fromJust . parseAggregationDWhat <$> oeCursors
+  print $ fromJust . parseTransactionDWhat <$> oeCursors
