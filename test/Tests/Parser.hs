00eb9d529484dab484206e96a712e0635fd4fb08
Matthew Perry <Matt.Perry@data61.csiro.au>
2017-12-12 16:56:35 +1100

    fix some bugs in Parser.hs tests
    Former-commit-id: 3d6881e31db221d117b4f7cd7d77d45129a7f69f

--- test/Tests/Parser.hs
+++ test/Tests/Parser.hs
@@ -36,13 +36,13 @@ testParser = do
       let tz1 = parseStr2TimeZone et1 :: Either EPCISTimeError TimeZone
       parseDWhen <$> oeCursors `shouldBe`
         [Right (DWhen (fromRight' t) (Just (fromRight' t)) (fromRight' tz)),
-        Right (DWhen (fromRight' t1) (Just (fromRight' t1)) (fromRight' tz1))]
+        Right (DWhen (fromRight' t1) Nothing (fromRight' tz1))]
 
     it "creates Nothing from Single ObjectEvent XML without Event Time" $ do
       doc <- Text.XML.readFile def "test/test-xml/ObjectEventNoEventTime.xml"
       let cursor = fromDocument doc
       let oeCursors = getCursorsByName "ObjectEvent" cursor
-      parseDWhen <$> oeCursors `shouldBe` [Left InvalidFormat]
+      parseDWhen <$> oeCursors `shouldBe` [Left TimeZoneError]
 
   describe "parse XML to obtain Action" $
     it "finds action from Single ObjectEvent XML" $ do
@@ -51,23 +51,22 @@ testParser = do
       let actions = cursor $// element "action" &/ content
       parseAction actions `shouldBe` Right Observe
 
-      -- TODO = check... [Just (ItemCount 1), Just (ItemCount 1)]. CHECK!!!
   describe "parse XML to obtain EPC List" $ do
     it "finds all epcs" $ do
       doc <- Text.XML.readFile def "test/test-xml/ObjectEventNoEventTime.xml"
       let cursor = fromDocument doc
       let epcs = cursor $// element "epc" &/ content
       --show <$> parseEPCList epcs Nothing `shouldBe`
-      show <$> parseEPCList epcs [Just (ItemCount 1), Just (ItemCount 1)] `shouldBe`
-        ["urn:epc:id:sgtin:0614141.107346.2017", "urn:epc:id:sgtin:0614141.107346.2018"]
+      parseEPCList epcs [Just (ItemCount 1), Just (ItemCount 1)] `shouldBe`
+        [Right $ IL $ SGTIN "0614141" Nothing "107346" "2017", Right $ IL $ SGTIN "0614141" Nothing "107346" "2018"]
 
     it "finds all child epcs" $ do
       doc <- Text.XML.readFile def "test/test-xml/AggregationEvent.xml"
       let cursor = fromDocument doc
       let epcs = cursor $// element "epc" &/ content
-      -- TODO = check
-      show <$> parseChildEPCList epcs [Just (ItemCount 1), Just (ItemCount 1)] `shouldBe`
-        ["urn:epc:id:sgtin:0614141.107346.2017", "urn:epc:id:sgtin:0614141.107346.2018"]
+      parseChildEPCList epcs [Just (ItemCount 1), Just (ItemCount 1)] `shouldBe`
+        [Right $ IL $ SGTIN "0614141" Nothing "107346" "2017", Right $ IL $ SGTIN "0614141" Nothing "107346" "2018"]
+        --["urn:epc:id:sgtin:0614141.107346.2017", "urn:epc:id:sgtin:0614141.107346.2018"]
 
   describe "parse XML to get BizStep" $ do
     it "find all the BizStep in multiple events XML" $ do
@@ -96,6 +95,7 @@ testParser = do
       let oeCursors = cursor $// element "ObjectEvent"
       parseDWhy <$> oeCursors `shouldBe` [Right $ DWhy (Just Shipping) (Just InTransit)]
 
+  -- following test - the Nothing extension used because a value of 0 indicates no extension
   describe "parse XML to obtain DWhere" $
     it "finds all the dwhere" $ do
       doc2 <- Text.XML.readFile def "test/test-xml/ObjectEvent.xml"
@@ -121,9 +121,9 @@ testParser = do
       doc <- Text.XML.readFile def "test/test-xml/ObjectEvent2.xml"
       let cursor = fromDocument doc
       let oeCursors = getCursorsByName "quantityElement" cursor
-      -- parseQuantityElement <$> oeCursors `shouldBe` [Just $ QuantityElement (EPCClass "urn:epc:class:lgtin:4012345.012345.998877") 200 (Just "KGM")]
       parseQuantity <$> oeCursors `shouldBe` [Just $ MeasuredQuantity 200 "KGM"]
 
+      -- TODO = check... may be incorrect!
   describe "parse BizTransaction" $ do
     it "parse BizTransaction element" $ do
       doc <- Text.XML.readFile def "test/test-xml/ObjectEvent.xml"
@@ -133,29 +133,27 @@ testParser = do
                                                    [Right BizTransaction {_btid = "http://transaction.acme.com/po/12345678", _bt = Po},
                                                      Right BizTransaction {_btid = "urn:epcglobal:cbv:bt:0614141073467:1152", _bt = Desadv}]]
 
-    it "get all attrs" $ do
-      doc2 <- Text.XML.readFile def "test/test-xml/ObjectEvent.xml"
-      let cursor = fromDocument doc2
-      let btCursor = cursor $// element "bizTransactionList"
-      let c = head btCursor
-      let attrs = c $/ element "bizTransaction" &| attribute "type"
-      mkBizTransactionType . T.unpack <$> foldMap id attrs `shouldBe` [Right Po]
+    -- TODO = fix. This is incorrect. should have been parseBizTransactionType but not available
+    -- it "get all attrs" $ do
+    --   doc2 <- Text.XML.readFile def "test/test-xml/ObjectEvent.xml"
+    --   let cursor = fromDocument doc2
+    --   let btCursor = cursor $// element "bizTransactionList"
+    --   let c = head btCursor
+    --   let attrs = c $/ element "bizTransaction" &| attribute "type"
+    --   mkBizTransactionType . T.unpack <$> foldMap id attrs `shouldBe` [Right Po]
 
   describe "parse DWhat" $ do
     it "parses a valid ObjectDWhat" $ do
       doc <- Text.XML.readFile def "test/test-xml/ObjectEvent2.xml"
       let cursor = fromDocument doc
       let oeCursors = getCursorsByName "ObjectEvent" cursor
-      -- parseObjectDWhat <$> oeCursors `shouldBe` [Right $ ObjectDWhat Observe [EPC "urn:epc:id:sgtin:0614141.107346.2017", EPC "urn:epc:id:sgtin:0614141.107346.2018"]] --[QuantityElement (EPCClass "urn:epc:class:lgtin:4012345.012345.998877") (200 :: Double)  (Just "KGM")]]
-      -- TODO = CHECK whether Nothing is appropriate below
+      -- NOTE the following test is correct, demonstrates bug in Parser
       parseObjectDWhat <$> oeCursors `shouldBe` [Right $ ObjectDWhat Observe [IL $ SGTIN "0614141" Nothing "107346" "2017", IL $ SGTIN "0614141" Nothing "107346" "2018"]]      
 
     it "parses a valid AggregationDWhat" $ do
       doc <- Text.XML.readFile def "test/test-xml/AggregationEvent.xml"
       let cursor = fromDocument doc
       let aeCursors = getCursorsByName "AggregationEvent" cursor
-      -- parseAggregationDWhat <$> aeCursors `shouldBe` [Right $ AggregationDWhat Observe (Just "urn:epc:id:sscc:0614141.1234567890") [EPC "urn:epc:id:sgtin:0614141.107346.2017", EPC "urn:epc:id:sgtin:0614141.107346.2018"] [QuantityElement (EPCClass "urn:epc:idpat:sgtin:4012345.098765.*") (10 :: Double) Nothing, QuantityElement (EPCClass "urn:epc:class:lgtin:4012345.012345.998877") (200.5 :: Double) (Just "KGM")]]
-      -- TODO = check Nothings are appropriate below
       parseAggregationDWhat <$> aeCursors `shouldBe` [Right $ AggregationDWhat Observe (Just $ IL $ SSCC "0614141" "1234567890") [IL $ SGTIN "0614141" Nothing "107346" "2017", IL $ SGTIN "0614141" Nothing "107346" "2018"]]
     -- it "parses a valid QuantityDWhat" $ do
     --   doc <- Text.XML.readFile def "test/test-xml/QuantityEvent.xml"
@@ -167,8 +165,6 @@ testParser = do
       doc <- Text.XML.readFile def "test/test-xml/TransactionEvent.xml"
       let cursor = fromDocument doc
       let teCursors = getCursorsByName "TransactionEvent" cursor
-      -- parseTransactionDWhat <$> teCursors `shouldBe` [Just (TransactionDWhat Observe Nothing [BizTransaction {_btid = "http://transaction.acme.com/po/12345678", _bt = Po}] [EPC "urn:epc:id:sgtin:0614141.107346.2017", EPC "urn:epc:id:sgtin:0614141.107346.2018"] []), Just (TransactionDWhat Observe Nothing [BizTransaction {_btid = "http://transaction.acme.com/po/12345678", _bt = Po},BizTransaction {_btid = "urn:epcglobal:cbv:bt:0614141073467:1152", _bt = Desadv}] [EPC "urn:epc:id:sgtin:0614141.107346.2018"] [])]
-      -- TODO = check Nothings are appropriate in below
       parseTransactionDWhat <$> teCursors `shouldBe` [Right (TransactionDWhat Observe Nothing [BizTransaction {_btid = "http://transaction.acme.com/po/12345678", _bt = Po}] [IL $ SGTIN "0614141" Nothing "107346" "2017", IL $ SGTIN "0614141" Nothing "107346" "2018"]), 
                                                       Right (TransactionDWhat Observe Nothing [BizTransaction {_btid = "http://transaction.acme.com/po/12345678", _bt = Po},BizTransaction {_btid = "urn:epcglobal:cbv:bt:0614141073467:1152", _bt = Desadv}] [IL $ SGTIN "0614141" Nothing "107346" "2017"])]
 
