589fd18aafeb11cd70b62ed4ed458348e0f39470
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2017-12-15 16:27:30 +1100

    Further tweaks

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,5 @@ stack clean
 stack build --coverage
 test_report=`stack test --coverage 2>&1 | egrep -i "The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at " | sed "s/The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at //g"`
 
-echo $test_report
 (google-chrome $test_report &)
 echo
