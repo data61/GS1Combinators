f9826f35a23f4485b9abb8d23351d78ae151a030
Matthew Perry <Matt.Perry@data61.csiro.au>
2017-12-15 18:01:44 +1100

    fix script bug

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ if [ $# -eq 1 ] && [ "$1" = "-c" ]; then
    stack clean
 fi
 
-test_file=`stack build --coverage; stack test --coverage 2>&1 | egrep -i "The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at " | sed "s/The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at //g"`;
+test_report=`stack build --coverage; stack test --coverage 2>&1 | egrep -i "The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at " | sed "s/The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at //g"`;
 
 
 (google-chrome $test_report &)
