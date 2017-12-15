da2fcf0968ca0fd717091cda976c1b533267943b
Matthew Perry <Matt.Perry@data61.csiro.au>
2017-12-15 17:55:42 +1100

    undo removal of either2maybe
    Former-commit-id: 989b7b6836941f1060956cd5e603a1eda0b8ef79

--- src/Data/GS1/Utils.hs
+++ src/Data/GS1/Utils.hs
@@ -55,9 +55,9 @@ parseURI s uri = let puri = T.pack uri
                         else Nothing
 
 -- returns (Just Right) or Nothing
--- either2Maybe :: Either a b -> Maybe b
--- either2Maybe (Right x) = Just x
--- either2Maybe (Left _) = Nothing
+either2Maybe :: Either a b -> Maybe b
+either2Maybe (Right x) = Just x
+either2Maybe (Left _) = Nothing
 
 flatten :: [[a]] -> [a]
 flatten xs = (\z n -> foldr (flip (foldr z)) n xs) (:) []
