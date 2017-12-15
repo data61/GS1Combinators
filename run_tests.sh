b0b82ad3fc3007e37acecb4ba8454b9eb867cc61
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2017-12-15 13:49:57 +1100

    Tweaks
    Former-commit-id: 3bbfd3d012602e40e4ffa619edd82c979898d4b0

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 # TODO add flags to decide whether or not to clean
-test_file=`stack clean; stack build --coverage; stack test --coverage 2>&1 | egrep -i "The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at " | sed "s/The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at //g"`
+stack clean
+stack build --coverage
+test_file=`stack test --coverage 2>&1 | egrep -i "The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at " | sed "s/The coverage report for GS1Combinators's test-suite \"GS1Combinators-test\" is available at //g"`
 
 echo $test_file
 (google-chrome $test_file &)
